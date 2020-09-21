286993ff92c20a53cbc4015d97d24efbbe2ede28
yani82 <yani.iman1@gmail.com>
2020-09-20 20:52:06 -0400

    Edit

--- lib/Mom_n_Pop/scraper.rb
+++ lib/Mom_n_Pop/scraper.rb
@@ -1,24 +1,50 @@
-require 'nokogiri'
-require 'open-uri'
+# require 'nokogiri'
+# require 'open-uri'
 
-class Scraper 
+# class Scraper 
 
-    @@local_business_url = 'https://www.yellowpages.com/new-york-ny/local-business'
-    @@business_name = []
-    @@business_category = []
-    @@last_search_page_scraped = [] 
+#     @@local_business_url = 'https://www.yellowpages.com/new-york-ny/local-business'
+#     @@business_listing = []
+#     @@business_details = []
+#     @@last_search_page_scraped = [] 
 
-    def self.get_local_business
-        local_businesses = Nokogiri::HTML(open(@@local_businesses_url))
-        business_name = business_name("span").first
-        @@business_category = business_category.css("a href").map {}
-        @@business_details = businesses_details.text.split
-    end 
+#     def self.get_local_businesses
+#         local_businesses = Nokogiri::HTML(open(@@local_businesses_url))
+#         list_of_local_businesses = list_of_local_businesses.css(div class="v-card").first
+#         @@business_listing = business_listing.css(div class="info").map 
+#         @@business_details = businesses_details.text("a class")
+#         @@business_details 
+#     end 
 
-    div class "v-card" 
-    div class "info" 
-    a class "business-name"  
-    span
+#     def self.business_listing  
+#         @@business_listing   
+#     end 
 
 
-end 
+#     def self.business_details
+#         @@business_details 
+#     end 
+
+
+#     def self.retreive_single_listing(url) 
+#         doc = Nokogiri::HTML(open(url))
+
+#         business_array = doc.css("").css("")
+#         business_name = doc.css("h1").text
+#         business_category = doc.css("a").map 
+#         business_phone = doc.css("p class")
+#         bussiness_address = doc.css("h2")
+#         general_info = ("dd class") 
+
+
+
+
+
+
+#     # div class "v-card" 
+#     # div class "info" 
+#     # a class "business-name"  
+#     # span
+
+
+# end 
