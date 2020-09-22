d9286a6c4ca15a0cb8a5e9ba40b1af31e39144a0
yani82 <yani.iman1@gmail.com>
2020-09-21 20:09:56 -0400

    back to scraping from Yellowpages

--- lib/Mom_n_Pop/scraper.rb
+++ lib/Mom_n_Pop/scraper.rb
@@ -1,40 +1,42 @@
-# require 'nokogiri'
-# require 'open-uri'
+require 'nokogiri'
+require 'open-uri'
 
-# class Scraper 
+class Scraper 
 
-#     @@local_business_url = 'https://www.yellowpages.com/new-york-ny/local-business'
-#     @@business_listing = []
-#     @@business_details = []
-#     @@last_search_page_scraped = [] 
+    @@local_business_url = 'https://www.yellowpages.com/new-york-ny/local-business'
+    @@business_listing = []
+    @@business_details = []
+    @@last_search_page_scraped = [] 
 
-#     def self.get_local_businesses
-#         local_businesses = Nokogiri::HTML(open(@@local_businesses_url))
-#         list_of_local_businesses = list_of_local_businesses.css(div class="v-card").first
-#         @@business_listing = business_listing.css(div class="info").map 
-#         @@business_details = businesses_details.text("a class")
-#         @@business_details 
-#     end 
+    def self.get_local_businesses
+        local_businesses = Nokogiri::HTML(open(@@local_business_url))
+        list_of_local_businesses = local_businesses.css(".v-card").map { |business| business }
+        @@business_listing = list_of_local_businesses.map { |info| info.css(".info-secondary")}
+        @@business_details = list_of_local_businesses.map { |info| info.css("a").attribute("href").value}
+        @@business_details 
+    end 
 
-#     def self.business_listing  
-#         @@business_listing   
-#     end 
-
-
-#     def self.business_details
-#         @@business_details 
-#     end 
+    def self.business_listing  
+        @@business_listing   
+    end 
 
 
-#     def self.retreive_single_listing(url) 
-#         doc = Nokogiri::HTML(open(url))
+    def self.business_details
+        @@business_details 
+    end 
 
-#         business_array = doc.css("").css("")
-#         business_name = doc.css("h1").text
-#         business_category = doc.css("a").map 
-#         business_phone = doc.css("p class")
-#         bussiness_address = doc.css("h2")
-#         general_info = ("dd class") 
+
+    def self.retreive_single_listing(url) 
+        doc = Nokogiri::HTML(open(url))
+
+        business_array = doc.css("").css("")
+        business_name = doc.css("h1").text
+        business_category = doc.css("a").map 
+        business_phone = doc.css("p class")
+        bussiness_address = doc.css("h2")
+        general_info = ("dd class") 
+    end 
+
 
 
 
@@ -47,4 +49,4 @@
 #     # span
 
 
-# end 
+end 
